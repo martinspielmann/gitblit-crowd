583b01f4e07bf3cd0519bc29b3edeb8fbadf825f
Remko Plantenga <sonata82@gmail.com>
2015-04-10 10:25:11 +0200

    Cookies for sale!

--- src/main/java/org/obiba/git/gitblit/ExternalCrowdUserService.java
+++ src/main/java/org/obiba/git/gitblit/ExternalCrowdUserService.java
@@ -1,8 +1,6 @@
 package org.obiba.git.gitblit;
 
-import com.atlassian.crowd.exception.ApplicationPermissionException;
-import com.atlassian.crowd.exception.InvalidAuthenticationException;
-import com.atlassian.crowd.exception.OperationFailedException;
+import com.atlassian.crowd.exception.*;
 import com.atlassian.crowd.integration.http.CrowdHttpAuthenticator;
 import com.atlassian.crowd.integration.http.CrowdHttpAuthenticatorImpl;
 import com.atlassian.crowd.integration.http.util.CrowdHttpTokenHelperImpl;
@@ -21,14 +19,19 @@ import com.gitblit.IUserService;
 import com.gitblit.manager.IRuntimeManager;
 import com.gitblit.models.TeamModel;
 import com.gitblit.models.UserModel;
+import com.gitblit.utils.StringUtils;
+import org.apache.wicket.protocol.http.WebRequestCycle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class ExternalCrowdUserService implements IUserService {
 
@@ -36,14 +39,13 @@ public class ExternalCrowdUserService implements IUserService {
     private static CrowdHttpAuthenticator authenticator;
     private static RepositoryPermissionsManager repo;
 
-    private final List<String> adminGroups = new ArrayList<String>();
+    private final List<String> adminGroups = new ArrayList<>();
     private static final Logger log = LoggerFactory.getLogger(ExternalCrowdUserService.class);
 
     private Properties loadCrowdProperties(final File crowdProperties) {
         Properties clientProperties = new Properties();
-        FileInputStream is = null;
-        try {
-            clientProperties.load(is = new FileInputStream(crowdProperties));
+        try (FileInputStream is = new FileInputStream(crowdProperties)) {
+            clientProperties.load(is);
             return clientProperties;
         } catch (FileNotFoundException e) {
             log.error("crowd.properties file {} does not exist.", crowdProperties.getAbsolutePath());
@@ -51,13 +53,6 @@ public class ExternalCrowdUserService implements IUserService {
         } catch (IOException e) {
             log.error("error reading crowd.properties file {}.", crowdProperties.getAbsolutePath());
             throw new RuntimeException(e);
-        } finally {
-            if (is != null) {
-                try {
-                    is.close();
-                } catch (IOException e) {
-                }
-            }
         }
     }
 
@@ -79,7 +74,6 @@ public class ExternalCrowdUserService implements IUserService {
             ExternalCrowdUserService.repo = new RepositoryPermissionsManager(permsFile);
 
             // Populate the list of groups with administrative privileges
-            // Populate the list of groups with administrative privileges
             List<String> admins = Arrays.asList(crowdProperties.getProperty("crowd.adminGroups").split(","));
             if (admins.isEmpty()) {
                 admins.add("administrators");
@@ -90,12 +84,26 @@ public class ExternalCrowdUserService implements IUserService {
 
     @Override
     public String getCookie(final UserModel model) {
-        // TODO Auto-generated method stub
-        return null;
+        // Return some unique value. To retrieve the user again, we use the token from Crowd so no need to do anything
+        // here
+        return StringUtils.getSHA1(model.getName());
     }
 
     @Override
     public UserModel getUserModel(final char[] cookie) {
+        WebRequestCycle requestCycle = (WebRequestCycle) WebRequestCycle.get();
+        if (requestCycle != null) {
+            HttpServletRequest request = requestCycle.getWebRequest().getHttpServletRequest();
+            HttpServletResponse response = requestCycle.getWebResponse().getHttpServletResponse();
+            try {
+                // Determines if the request has a valid SSO token
+                if (authenticator.isAuthenticated(request, response)) {
+                    return CrowdUtils.mapCrowdUserToModel(client, repo, authenticator.getUser(request).getName());
+                }
+            } catch (CrowdException|ApplicationPermissionException|InvalidTokenException e) {
+                log.error("error while trying to validate token", e);
+            }
+        }
         return null;
     }
 
@@ -132,36 +140,26 @@ public class ExternalCrowdUserService implements IUserService {
     @Override
     public List<String> getAllUsernames() {
         try {
-            return client.searchUserNames(new TermRestriction<Boolean>(new PropertyImpl<Boolean>("active", Boolean.class),
+            return client.searchUserNames(new TermRestriction<>(new PropertyImpl<>("active", Boolean.class),
                     true), 0, 4096);
-        } catch (OperationFailedException e) {
-            e.printStackTrace();
-        } catch (InvalidAuthenticationException e) {
-            e.printStackTrace();
-        } catch (ApplicationPermissionException e) {
-            e.printStackTrace();
+        } catch (OperationFailedException|InvalidAuthenticationException|ApplicationPermissionException e) {
+            log.error("error while fetching all user names", e);
         }
-        return new ArrayList<String>();
+        return new ArrayList<>();
     }
 
     @Override
     public List<UserModel> getAllUsers() {
-        List<User> searchUsers = new ArrayList<User>();
-        List<UserModel> result = new ArrayList<UserModel>();
+        List<User> searchUsers = new ArrayList<>();
+        List<UserModel> result = new ArrayList<>();
         try {
-            searchUsers = client.searchUsers(new TermRestriction<Boolean>(new PropertyImpl<Boolean>("active",
+            searchUsers = client.searchUsers(new TermRestriction<>(new PropertyImpl<>("active",
                     Boolean.class), true), 0, 4096);
-        } catch (OperationFailedException e) {
-            e.printStackTrace();
-        } catch (InvalidAuthenticationException e) {
-            e.printStackTrace();
-        } catch (ApplicationPermissionException e) {
-            e.printStackTrace();
+        } catch (OperationFailedException|InvalidAuthenticationException|ApplicationPermissionException e) {
+            log.error("error while fetching all users", e);
         }
 
-        for (User u : searchUsers) {
-            result.add(CrowdUtils.mapCrowdUserToModel(client, repo, u));
-        }
+        result.addAll(searchUsers.stream().map(u -> CrowdUtils.mapCrowdUserToModel(client, repo, u)).collect(Collectors.toList()));
 
         return result;
     }
@@ -169,16 +167,11 @@ public class ExternalCrowdUserService implements IUserService {
     @Override
     public List<String> getAllTeamNames() {
         try {
-            List<String> searchGroupNames = client.searchGroupNames(NullRestrictionImpl.INSTANCE, 0, 4096);
-            return searchGroupNames;
-        } catch (OperationFailedException e) {
-            e.printStackTrace();
-        } catch (InvalidAuthenticationException e) {
-            e.printStackTrace();
-        } catch (ApplicationPermissionException e) {
-            e.printStackTrace();
+            return client.searchGroupNames(NullRestrictionImpl.INSTANCE, 0, 4096);
+        } catch (OperationFailedException|InvalidAuthenticationException|ApplicationPermissionException e) {
+            log.error("error while fetching all team names", e);
         }
-        return new ArrayList<String>();
+        return new ArrayList<>();
     }
 
     @Override
@@ -186,52 +179,39 @@ public class ExternalCrowdUserService implements IUserService {
 
         try {
             List<Group> searchGroups = client.searchGroups(NullRestrictionImpl.INSTANCE, 0, 4096);
-            List<TeamModel> result = new ArrayList<TeamModel>();
+            List<TeamModel> result = new ArrayList<>();
             for (Group g : searchGroups) {
                 TeamModel teamModel = new TeamModel(g.getName());
                 teamModel.accountType = AccountType.EXTERNAL;
                 result.add(teamModel);
 
-                for (TeamModel repoTeam : repo.getTeamModels()) {
-                    if (teamModel.name.equals(repoTeam.name)) {
-                        teamModel.permissions.putAll(repoTeam.permissions);
-                        teamModel.canAdmin = repoTeam.canAdmin;
-                        teamModel.canCreate = repoTeam.canCreate;
-                        teamModel.canFork = repoTeam.canFork;
-                    }
-                }
-
+                repo.getTeamModels().stream().filter(repoTeam -> teamModel.name.equals(repoTeam.name)).forEach(repoTeam -> {
+                    teamModel.permissions.putAll(repoTeam.permissions);
+                    teamModel.canAdmin = repoTeam.canAdmin;
+                    teamModel.canCreate = repoTeam.canCreate;
+                    teamModel.canFork = repoTeam.canFork;
+                });
             }
             return result;
-        } catch (OperationFailedException e) {
-            e.printStackTrace();
-        } catch (InvalidAuthenticationException e) {
-            e.printStackTrace();
-        } catch (ApplicationPermissionException e) {
-            e.printStackTrace();
+        } catch (OperationFailedException|InvalidAuthenticationException|ApplicationPermissionException e) {
+            log.error("error while fetching all teams", e);
         }
-        return new ArrayList<TeamModel>();
+        return new ArrayList<>();
     }
 
     @Override
     public List<String> getTeamNamesForRepositoryRole(final String role) {
-        // return repo.repoTeams(role);
         try {
             return client.searchGroupNames(NullRestrictionImpl.INSTANCE, 0, 4096);
-        } catch (OperationFailedException e) {
-            e.printStackTrace();
-        } catch (InvalidAuthenticationException e) {
-            e.printStackTrace();
-        } catch (ApplicationPermissionException e) {
-            e.printStackTrace();
+        } catch (OperationFailedException|InvalidAuthenticationException|ApplicationPermissionException e) {
+            log.error("error while fetching all team names for the role {}", role, e);
         }
-        return new ArrayList<String>();
+        return new ArrayList<>();
     }
 
     @Override
     public TeamModel getTeamModel(final String teamname) {
-        TeamModel team = new TeamModel(teamname);
-        return team;
+        return new TeamModel(teamname);
     }
 
     @Override
